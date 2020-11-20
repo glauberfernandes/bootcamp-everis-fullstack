d70545c4099be63cc093e5ebaa2992751d354071
Glauber Fernandes <glaubersfernandes@gmail.com>
2020-11-20 11:05:25 -0300

    Adicionado a saída do module

--- JavaScript_ES6_Essencial/DesignPatterns/module/2-module-import.js
+++ JavaScript_ES6_Essencial/DesignPatterns/module/2-module-import.js
@@ -3,3 +3,7 @@ const {getName, setName} = require('./1-module');
 console.log(getName());
 console.log(setName('Glauber'));
 console.log(getName());
+
+// default
+// undefined (retorno do setName)
+// Glauber
