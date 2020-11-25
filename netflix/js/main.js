00623624500437da4faf22583bb0298e1b50b264
Glauber Fernandes <glaubersfernandes@gmail.com>
2020-11-24 21:42:53 -0300

    Clone do NetFlix - part 8 e 9

---
+++ netflix/js/main.js
@@ -0,0 +1,7 @@
+function maisInfo(){
+    alert('Essa série tá muito top...');
+}
+
+function assistirAgora(){
+    open("https://www.youtube.com/watch?v=SvSkxBYuoQY&t=15s", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
