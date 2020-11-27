1898bafc8288e8f29a5d5a5b855be2972b7bc8d2
Glauber Fernandes <glaubersfernandes@gmail.com>
2020-11-26 21:45:03 -0300

    Desmistificando reduce - part 5

---
+++ Desmistificando_Map_Filter_Reduce/reduce/2-exampleReduce.js
@@ -0,0 +1,47 @@
+const pets = [
+    {
+        type: 'dog',
+        name: 'bolinha',
+        age: 15,
+        weight: 30
+    },
+    {
+        type: 'cat',
+        name: 'mingal',
+        age: 6,
+        weight: 2
+    },
+    {
+        type: 'dog',
+        name: 'rex',
+        age: 4,
+        weight: 5
+    },
+    {
+        type: 'cat',
+        name: 'marrom',
+        age: 2,
+        weight: 1
+    },
+    {
+        type: 'fish',
+        name: 'gulp',
+        age: 1,
+        weight: 1
+    },
+    {
+        type: 'horse',
+        name: 'pé de pano',
+        age: 1,
+        weight: 1
+    }
+]
+
+const totalWeight = pets.reduce((total, pet) => {
+    return {
+        totalAge: total.totalAge + pet.age,
+        totalWeight: total.totalWeight + pet.weight 
+    }
+}, { totalAge: 0, totalWeight: 0 });
+
+console.log(totalWeight);
