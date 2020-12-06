545f621f378ebd8936b71548f72492f24b1fc9fb
Glauber Fernandes <glaubersfernandes@gmail.com>
2020-12-06 15:58:44 -0300

    Entendendo Promises

--- EntendendoPromises/index.js
+++ EntendendoPromises/index.js
@@ -2,9 +2,31 @@ const fs = require('fs');
 const path = require('path');
 const basePath = './assets/';
 
+function cb (err, data) {
+    if (err) throw err
+    console.log(data);
+}
+
 console.log('Begin');
 
-const content = fs.readFileSync(path.resolve(basePath, 'invictus.txt'))
-console.log(content.toString());
+//const files = fs.readdirSync(path.resolve(basePath));
+//
+//const sentences = files.filter((file) => /s[1-4].txt/gi.test(file));
+//
+//for (const sentence of sentences) {
+//    const text = fs.readFileSync(path.resolve(basePath, sentence)).toString();
+//
+//    console.log(text, '\n');
+//}
+
+//fs.readFile(path.resolve(basePath, 'invictus.txt'), { encoding: 'utf-8' }, cb);
+
+const files = fs.readdirSync(path.resolve(basePath));
+
+const sentences = files.filter((file) => /s[1-4].txt/gi.test(file));
+
+for (const file of sentences) {
+    fs.readFile(path.resolve(basePath, file), {encoding: 'utf-8'}, cb);
+}
 
 console.log('End');
